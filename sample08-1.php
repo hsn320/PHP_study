832063f68ef9451a6ca2ac03651fdcef94d2ceb9
hsn320 <2250025@ecc.ac.jp>
2026-07-03 13:31:53 +0900

    js

--- sample08-1.php
+++ sample08-1.php
@@ -5,5 +5,7 @@ require_once __DIR__ . "/kadai02_resource.php";
 
 // コンテンツタイプをJSONに設定
 header("Content-Type: application/json; charset=UTF-8");
+// クロスオリジンの許可
+header("Access-Control-Allow-Origin: *");
 // JSONへ変換
 print json_encode($products);
